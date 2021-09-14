ba759a0f158fa47a36fd15a5610cd9f3fbf08347
Josh Willox <joshwillox@gmail.com>
2021-09-14 23:12:40 +1000

    Add cors middleware

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,8 +1,10 @@
+import cors from "cors";
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
 app.use(express.static("build"));
+app.use(cors());
 
 app.get("*", (req: Request, res: Response) => {
   res.sendFile("build/index.html", { root: "." });
