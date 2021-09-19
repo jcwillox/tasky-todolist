42aae620f1265db3b6664b3a1df7e41639ede5d1
Josh Willox <joshwillox@gmail.com>
2021-09-19 21:19:44 +1000

    Implement `/login` and `/register` API endpoints

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
+import users from "./routes/users";
 
 const { NODE_ENV } = process.env;
 const app: Express = express();
@@ -11,6 +12,7 @@ if (NODE_ENV !== "production" && NODE_ENV !== "test") {
 
 app.use(express.static("build"));
 app.use(cors());
+app.use("/api", users);
 
 /** fallback route for all api requests */
 app.get("/api*", (req: Request, res: Response) => {
---
+++ src/api/routes/users.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response, Router } from "express";
+import { NewUser } from "../../models/user";
+import jwt from "jsonwebtoken";
+import User from "../database/models/users";
+import { ValidationError } from "sequelize";
+import { LoginBody } from "../../models/login";
+
+const SECRET_KEY = process.env.SECRET_KEY || "test_secret_key";
+
+const router = Router();
+
+router.use(express.json());
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { password, username }: LoginBody = req.body;
+
+  const user = await User.findOne({
+    where: {
+      username
+    }
+  });
+
+  if (user && (await user.validatePassword(password)))
+    return res.json({
+      ...user.details(),
+      token: jwt.sign({ id: user.id }, SECRET_KEY)
+    });
+
+  return res.sendStatus(401);
+});
+
+router.post("/register", async (req: Request, res: Response) => {
+  const body: NewUser = req.body;
+
+  try {
+    await User.create(body);
+    res.sendStatus(200);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      res.status(422).json({
+        errors: err.errors.map(item => ({
+          attribute: item.path,
+          message: item.message
+        }))
+      });
+    } else {
+      console.error("'/register':", err);
+      res.sendStatus(400);
+    }
+  }
+});
+
+export default router;
