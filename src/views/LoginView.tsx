b934861c6a9a08485c9b094479b228fde1523ed8
Kent842 <69133925+Kent842@users.noreply.github.com>
2021-10-05 21:52:07 +1100

    Add login view (#3)
    * Added login view
    
    * Implemented Formik components

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,7 +1,108 @@
-import React from "react";
+import AppFormTitle from "../components/AppFormTitle";
+import AppView from "../components/AppView";
+import { Form, Formik } from "formik";
+import { Box, Button } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { LoginBodySchema } from "../schemas";
+import { useAuth } from "../components/AuthContext";
+import FormikTextField from "../components/FormikTextField";
+import FormikPasswordField from "../components/FormikPasswordField";
+import { useAsyncError } from "../hooks/use-async";
+
+const textFieldStyle = {
+  width: 328,
+  mt: 3
+};
+
+const centered = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
 
 const LoginView = () => {
-  return <div>LoginView</div>;
+  const auth = useAuth();
+  const wrapAsync = useAsyncError();
+
+  return (
+    <AppView>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          border: theme => `2px solid ${theme.palette.primary.main}`,
+          borderRadius: 3,
+          maxWidth: 688,
+          maxHeight: 700
+        }}
+      >
+        <AppFormTitle title="Sign In" />
+        <Formik
+          initialValues={{
+            username: "",
+            password: ""
+          }}
+          validationSchema={LoginBodySchema}
+          onSubmit={async values => {
+            await wrapAsync(auth.login(values));
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              {/* Textfields container */}
+              <Box sx={{ ...centered, flexDirection: "column" }}>
+                <FormikTextField
+                  name="username"
+                  label="Username"
+                  placeholder="john.smith"
+                  sx={textFieldStyle}
+                />
+                <FormikPasswordField
+                  name="password"
+                  label="Password"
+                  placeholder="Password"
+                  sx={textFieldStyle}
+                />
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    width: "100%"
+                  }}
+                >
+                  <Button
+                    href="/register"
+                    sx={{
+                      mt: 2,
+                      alignSelf: "flex-start",
+                      paddingLeft: "0px"
+                    }}
+                  >
+                    Create Account
+                  </Button>
+
+                  <LoadingButton
+                    type="submit"
+                    variant="contained"
+                    loading={isSubmitting}
+                    sx={{
+                      mt: 2,
+                      alignSelf: "flex-end"
+                    }}
+                  >
+                    {(isSubmitting && "") || "Sign In"}
+                  </LoadingButton>
+                </Box>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </AppView>
+  );
 };
 
 export default LoginView;
