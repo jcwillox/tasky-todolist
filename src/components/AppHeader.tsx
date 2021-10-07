0bcf3e7e365d812b86844606c9722f1258f0784a
Josh Willox <joshwillox@gmail.com>
2021-10-07 15:59:45 +1100

    Make AppHeader link to default page

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Box, styled, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Link, styled, Toolbar, Typography } from "@mui/material";
 import { ReactComponent as LogoIcon } from "../assets/logo.svg";
 import SettingsMenu from "./SettingsMenu";
 
@@ -15,27 +15,31 @@ const AppHeader = () => {
       }}
     >
       <Toolbar>
-        <Box
-          sx={{
-            marginLeft: -1.5,
-            marginRight: 2,
-            padding: 0.8,
-            width: 42,
-            height: 42,
-            boxShadow: 4,
-            backgroundColor: "primary.main",
-            borderRadius: 1.5
-          }}
-        >
-          <LogoIcon
-            style={{
-              filter: "drop-shadow(3px 3px 3px rgb(0 0 0 / 0.2))"
+        <Link href="/" color="inherit" underline="none">
+          <Box
+            sx={{
+              marginLeft: -1.5,
+              marginRight: 2,
+              padding: 0.8,
+              width: 42,
+              height: 42,
+              boxShadow: 4,
+              backgroundColor: "primary.main",
+              borderRadius: 1.5
             }}
-          />
-        </Box>
-        <Typography variant="h6" noWrap>
-          Tasky
-        </Typography>
+          >
+            <LogoIcon
+              style={{
+                filter: "drop-shadow(3px 3px 3px rgb(0 0 0 / 0.2))"
+              }}
+            />
+          </Box>
+        </Link>
+        <Link href="/" color="inherit" underline="none">
+          <Typography variant="h6" noWrap>
+            Tasky
+          </Typography>
+        </Link>
         <Spacer />
         <SettingsMenu />
       </Toolbar>
