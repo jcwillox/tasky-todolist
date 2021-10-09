5c357409e1c859f29d63592e4e59a95a96b8f229
Josh Willox <joshwillox@gmail.com>
2021-10-09 15:54:36 +1100

    Ensure we use an in-memory database during CI
    This prevents issues with concurrent access.

--- src/api/database/database.ts
+++ src/api/database/database.ts
@@ -1,10 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
-import { SyncOptions } from "sequelize/types/lib/sequelize";
+import { SyncOptions } from "sequelize";
 
 let sequelize: Sequelize | null;
 
 const connect = async () => {
-  const url = process.env.DATABASE_URL || "sqlite::memory:";
+  const url =
+    process.env.DATABASE_URL && !process.env.CI
+      ? process.env.DATABASE_URL
+      : "sqlite::memory:";
 
   sequelize = new Sequelize(url, {
     logging: false,
