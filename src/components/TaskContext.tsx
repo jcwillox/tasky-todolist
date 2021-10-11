974ba36045f9eba7c28fd64e2a745ce734ee2874
Josh Willox <joshwillox@gmail.com>
2021-10-11 16:34:30 +1100

    Deserialize dates from tasks response

--- src/components/TaskContext.tsx
+++ src/components/TaskContext.tsx
@@ -3,19 +3,43 @@ import React, {
   FunctionComponent,
   useCallback,
   useContext,
+  useEffect,
   useState
 } from "react";
 import { apiJSON } from "../utils/fetch";
 import { Task } from "../models/task";
 import { useLocalStorageState } from "use-local-storage-state";
 
+/** Deserialize date fields from strings to Date objects */
+const parseDates = (json: any[]) => {
+  for (const o of json)
+    for (const field of ["dueAt", "createdAt", "updatedAt"])
+      o[field] && (o[field] = new Date(o[field]));
+  return json;
+};
+
 const useProvideTasks = () => {
-  const [tasks, setTasks] = useLocalStorageState<Task[]>("tasks", []);
+  const [localTasks, setLocalTasks] = useLocalStorageState<Task[]>("tasks", []);
+  const [tasks, setTasks] = useState<Task[]>(parseDates(localTasks));
   const [isReloading, setIsReloading] = useState(false);
+
+  // the following use effect hooks facilitate syncing tasks with local storage
+  // while also deserializing the task dates when retrieved from local storage
+  useEffect(() => {
+    if (localTasks !== tasks) {
+      setTasks(parseDates(localTasks));
+    }
+  }, [localTasks]); // eslint-disable-line
+
+  useEffect(() => {
+    if (localTasks !== tasks) {
+      setLocalTasks(tasks);
+    }
+  }, [tasks]); // eslint-disable-line
 
   const reload = useCallback(async () => {
     setIsReloading(true);
-    setTasks(await apiJSON("/tasks"));
+    setTasks(parseDates(await apiJSON("/tasks")));
     setIsReloading(false);
   }, [setTasks]);
 
