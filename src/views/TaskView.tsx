55157a019d7f619a62601df993f845bca47bb73b
Haohang Zhou <zhouhaohangau@gmail.com>
2021-10-11 17:54:53 +1100

    removed unused import

--- src/views/TaskView.tsx
+++ src/views/TaskView.tsx
@@ -1,21 +1,7 @@
-import {
-  Box,
-  Button,
-  Divider,
-  List,
-  TextField,
-  Typography
-} from "@mui/material";
+import { Box, Divider, List, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import AppTask from "../components/AppTask";
-import { Form, Formik } from "formik";
 import { useTasks } from "../components/TaskContext";
-import { TaskSchema } from "../schemas/tasks";
-import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import AddIcon from "@mui/icons-material/Add";
-import FormikTextField from "../components/FormikTextField";
 
 const TaskView = () => {
   const { tasks, reload } = useTasks();
