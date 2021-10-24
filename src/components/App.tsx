8aa0f4c53fa25ad4b6ff99ba1b428a3c62dd5ecd
Josh Willox <joshwillox@gmail.com>
2021-10-24 23:01:00 +1100

    Use LinearProgress bar when lazy loading view

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,31 @@ import React, { Suspense } from "react";
 import AppHeader from "./AppHeader";
 import AppContent from "./AppContent";
 import { ProvideAuth } from "./AuthContext";
-import { Container, CssBaseline, styled, Theme } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  LinearProgress,
+  styled,
+  Theme
+} from "@mui/material";
 import { SxProps } from "@mui/system";
 import ThemeModeProvider from "./ThemeModeContext";
 import { BrowserRouter } from "react-router-dom";
 import { TaskProvider } from "./TaskContext";
 
 const AppbarOffset = styled("div")(({ theme }) => theme.mixins.toolbar);
+
+const LoadingBar = styled(props => (
+  <div {...props}>
+    <AppbarOffset />
+    <LinearProgress />
+  </div>
+))({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%"
+});
 
 const containerStyle: SxProps<Theme> = {
   height: "100vh",
@@ -30,7 +48,7 @@ function App() {
             <AppHeader />
             <Container maxWidth="md" disableGutters sx={containerStyle}>
               <AppbarOffset />
-              <Suspense fallback={<div>Loading...</div>}>
+              <Suspense fallback={<LoadingBar />}>
                 <AppContent />
               </Suspense>
             </Container>
